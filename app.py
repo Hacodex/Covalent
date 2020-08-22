ab4cb3f9b55ebbac62430c795c00c4bfc7826212
James Chu <jamesdchu@gmail.com>
2020-08-22 01:06:44 -0400

    Initialization

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-from flask import render_template
-from flask import request
-# from flask_pymongo import PyMongo
-
+from flask import render_template, redirect, session, url_for, request
+from flask_pymongo import PyMongo
+import bcrypt
+import model
+import os
 
 # -- Initialization section --
 app = Flask(__name__)
-
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+app.secret_key = "9AA6Ghg6je.EhDW3fe.34mkm4NJ"
+user = os.environ['user']
+pw = os.environ['pw']
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'MHacks2020'
 
-# URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+#URI of database
+app.config['MONGO_URI'] = f'mongodb+srv://{user}:{pw}@cluster0.6gvi8.mongodb.net/MHacks2020?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
